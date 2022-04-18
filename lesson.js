3e2c896a409651f7559ae22ecbdd5b389b0d114f
unknown <kane24051@gmail.com>
2022-04-18 23:27:13 +0300

    first commit

---
+++ lesson.js
@@ -0,0 +1,30 @@
+let yolochka4 = 4; // тип данных number 
+let yolochka6 = 6; // тип данных number 
+let yolochka_name = "Zelyonaya" // тип данных Строка
+
+let result = yolochka4 + yolochka6;
+let result_2 = yolochka4 + yolochka_name;
+console.log(result);
+console.log(result_2);
+console.log(result + yolochka4 + yolochka6);
+
+let t = true; // тип данных буллинг, true = 1  
+let f = false; // тип данных буллинг, false = 0 
+
+// console.log(t + f);
+
+if (f) {
+    let apples = 3+5
+    console.log("If true = " + apples)
+} 
+else if (yolochka4 + yolochka6 == 11){
+    let water = 15
+    console.log("Else if 1 =" + water)
+}
+else if (yolochka4 + yolochka6 == 10){
+    let milk = 2
+    console.log("Else if 2 =" + milk)}
+else {
+    let meat = 2+2
+    console.log("If not true = " + meat)
+}
